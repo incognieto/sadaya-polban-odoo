bffb87a2f8460d66d4e448bd1a7d7f36508fa372
WeirdoKitten <satriapermata12@gmail.com>
2026-05-19 21:16:50 +0700

    hapus file lama

--- custom_addons/sidapet_vendor/models/fasilitas.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-
-class SidapetFasilitas(models.Model):
-	_name = 'sidapet.fasilitas'
-	_description = 'Fasilitas'
-
-	penyedia_id = fields.Many2one('sidapet.penyedia', required=True, ondelete='cascade')
-	name = fields.Char()
--- custom_addons/sidapet_vendor/models/kbli.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class KBLI(models.Model):
-    _name = 'sidapet.kbli'
-
-    izin_id = fields.Many2one('sidapet.izin.usaha', required=True, ondelete='cascade')
-    kode_kbli = fields.Char()
