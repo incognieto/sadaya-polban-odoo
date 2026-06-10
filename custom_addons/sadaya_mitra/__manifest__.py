8ec8904342edfae34c4fb32814f1c879bf1b4ac6
jzdnnn <jzdnnn91@gmail.com>
2026-06-10 22:27:55 +0700

    fix: ui change for sadaya-mitra

--- custom_addons/sadaya_mitra/__manifest__.py
+++ custom_addons/sadaya_mitra/__manifest__.py
@@ -11,6 +11,11 @@
         'views/website_penyedia_templates.xml',
         'views/website_lanjutan_templates.xml',
     ],
+    'assets': {
+        'web.assets_frontend': [
+            'sadaya_mitra/static/src/scss/sadaya_mitra_frontend.scss',
+        ],
+    },
     'installable': True,
     'application': True,
 }
